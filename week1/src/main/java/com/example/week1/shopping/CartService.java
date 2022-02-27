67006fda28a26ef74f2bb57d4dd67f5ea4367ec7
D1-PC <polubu@gmail.com>
2022-02-27 15:14:59 +0700

    fix : Fix add cart method to support add cart item

--- week1/src/main/java/com/example/week1/shopping/CartService.java
+++ week1/src/main/java/com/example/week1/shopping/CartService.java
@@ -2,15 +2,11 @@ package com.example.week1.shopping;
 
 import com.example.week1.product.Product;
 import com.example.week1.product.ProductRepository;
-import com.example.week1.user.User;
 import com.example.week1.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.sql.Timestamp;
-import java.util.Optional;
 
 @Service
 public class CartService {
@@ -19,24 +15,24 @@ public class CartService {
     ShoppingSessionRepository shoppingSessionRepository;
     @Autowired
     CartItemRepository cartItemRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ProductRepository productRepository;
 
     public CartItem addCart(int userId, int productId, int quantity){
-        User user = new User();
-        user.setId(userId);
         ShoppingSession shoppingSession = new ShoppingSession();
-        shoppingSession.setUser(user);
+        shoppingSession.setUser(userRepository.findById(userId).get());
         shoppingSession.setModifiedAt(new Timestamp(System.currentTimeMillis()));
         shoppingSession.setCreatedAt(new Timestamp(System.currentTimeMillis()));
         shoppingSession = shoppingSessionRepository.save(shoppingSession);
-        Product product = new Product();
-        product.setId(productId);
+        Product product = productRepository.findById(productId).get();
         CartItem cartItem = new CartItem();
-        cartItem.setProduct(product);
+        cartItem.setCartItem(product);
         cartItem.setQuantity(quantity);
         cartItem.setCreatedAt(new Timestamp(System.currentTimeMillis()));
         cartItem.setShoppingSession(shoppingSession);
         cartItem = cartItemRepository.save(cartItem);
-
         return cartItem;
     }
 }
