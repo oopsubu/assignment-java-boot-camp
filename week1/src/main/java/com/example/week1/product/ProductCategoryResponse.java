4362c175e4b5aa677972852a7165f0faa5a118fd
D1-PC <polubu@gmail.com>
2022-02-27 20:18:01 +0700

    feat : Add ProductCategoryResponse for response data

---
+++ week1/src/main/java/com/example/week1/product/ProductCategoryResponse.java
@@ -0,0 +1,75 @@
+package com.example.week1.product;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+public class ProductCategoryResponse {
+	private int id;
+	private String name;
+	private String desc;
+	private Timestamp createdAt;
+	private Timestamp modifiedAt;
+	private Timestamp deletedAt;
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setCreatedAt(Timestamp createdAt){
+		this.createdAt = createdAt;
+	}
+
+	public Timestamp getCreatedAt(){
+		return createdAt;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setModifiedAt(Timestamp modifiedAt){
+		this.modifiedAt = modifiedAt;
+	}
+
+	public Timestamp getModifiedAt(){
+		return modifiedAt;
+	}
+
+	public void setDeletedAt(Timestamp deletedAt){
+		this.deletedAt = deletedAt;
+	}
+
+	public Timestamp getDeletedAt(){
+		return deletedAt;
+	}
+
+	public void setDesc(String desc){
+		this.desc = desc;
+	}
+
+	public String getDesc(){
+		return desc;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ProductCategory{" + 
+			"name = '" + name + '\'' + 
+			",created_at = '" + createdAt + '\'' + 
+			",id = '" + id + '\'' + 
+			",modified_at = '" + modifiedAt + '\'' + 
+			",deleted_at = '" + deletedAt + '\'' + 
+			",desc = '" + desc + '\'' + 
+			"}";
+		}
+}
