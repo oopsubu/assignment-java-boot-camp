30cec3449aa570c047dfa7481e49da3cf748dcf8
D1-PC <polubu@gmail.com>
2022-02-27 15:15:30 +0700

    fix : Add API get cart by id

--- week1/src/main/java/com/example/week1/shopping/CartController.java
+++ week1/src/main/java/com/example/week1/shopping/CartController.java
@@ -1,35 +1,30 @@
 package com.example.week1.shopping;
 
-import com.example.week1.product.Product;
-import com.example.week1.product.ProductRepository;
-import com.example.week1.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 public class CartController {
 
     @Autowired
     CartService cartService;
+    @Autowired
+    CartItemRepository cartItemRepository;
 
     @PostMapping("/cart/add/{productId}/{userId}/{quantity}")
-    public Map<String, Object> addCart(@PathVariable("productId") int productId, @PathVariable("userId") int userId, @PathVariable("quantity") int quantity){
+    public ResponseEntity<CartItem> addCart(@PathVariable("productId") int productId, @PathVariable("userId") int userId, @PathVariable("quantity") int quantity){
         CartItem cartItem = cartService.addCart(userId, productId, quantity);
-        Map<String, Object> response = new HashMap<String, Object>();
-        response.put("product", cartItem.getProduct());
-        response.put("userPayment", cartItem.getShoppingSession().getUser().getUserPayments());
-//        response.put("userAddress", cartItem.getShoppingSession().getUser().getAddress());
-        response.put("quantity", cartItem.getQuantity());
-        return response;
+        return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/cart/{cartId}")
+    public ResponseEntity<CartItem> getCart(@PathVariable("cartId") int cartId){
+        CartItem cartItem = cartItemRepository.findById(cartId).get();
+        return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
     }
 }
