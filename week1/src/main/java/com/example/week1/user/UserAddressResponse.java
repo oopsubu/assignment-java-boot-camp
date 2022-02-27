cab3aef8df2c9f175116653a1fdb6aaf2138aff7
D1-PC <polubu@gmail.com>
2022-02-27 20:17:14 +0700

    feat : Add UserAddressResponse for response data

---
+++ week1/src/main/java/com/example/week1/user/UserAddressResponse.java
@@ -0,0 +1,92 @@
+package com.example.week1.user;
+
+import javax.persistence.*;
+
+public class UserAddressResponse {
+	private int id;
+	private String country;
+	private String addressLine2;
+	private String addressLine1;
+	private String city;
+	private String postalCode;
+	private String mobile;
+	private String telephone;
+
+	public void setCountry(String country){
+		this.country = country;
+	}
+
+	public String getCountry(){
+		return country;
+	}
+
+	public void setAddressLine2(String addressLine2){
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getAddressLine2(){
+		return addressLine2;
+	}
+
+	public void setAddressLine1(String addressLine1){
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine1(){
+		return addressLine1;
+	}
+
+	public void setCity(String city){
+		this.city = city;
+	}
+
+	public String getCity(){
+		return city;
+	}
+
+	public void setMobile(String mobile){
+		this.mobile = mobile;
+	}
+
+	public String getMobile(){
+		return mobile;
+	}
+
+	public void setTelephone(String telephone){
+		this.telephone = telephone;
+	}
+
+	public String getTelephone(){
+		return telephone;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setPostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+
+	public String getPostalCode(){
+		return postalCode;
+	}
+
+ 	public String toString(){
+		return 
+			"UserAddress{" + 
+			"country = '" + country + '\'' + 
+			",address_line2 = '" + addressLine2 + '\'' +
+			",address_line1 = '" + addressLine1 + '\'' + 
+			",city = '" + city + '\'' + 
+			",mobile = '" + mobile + '\'' + 
+			",telephone = '" + telephone + '\'' + 
+			",id = '" + id + '\'' + 
+			",postal_code = '" + postalCode + '\'' + 
+			"}";
+		}
+}
