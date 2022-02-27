f336edd8dc0c59bcd6164170b8ca93524cfa4562
D1-PC <polubu@gmail.com>
2022-02-27 20:21:54 +0700

    feat : Modify ProductController change response from entity to response class.

--- week1/src/main/java/com/example/week1/product/ProductController.java
+++ week1/src/main/java/com/example/week1/product/ProductController.java
@@ -1,12 +1,15 @@
 package com.example.week1.product;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -14,15 +17,19 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("/product/list")
-    public List<Product> getProducts(){
-        List<Product> productList = productRepository.findAll();
+    public List<ProductResponse> getProducts(){
+        List<ProductResponse> productList = productRepository.findAll().stream().map(post -> modelMapper.map(post, ProductResponse.class))
+                .collect(Collectors.toList());
         return productList;
     }
 
     @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable int id){
+    public ProductResponse getProductById(@PathVariable int id){
         Optional<Product> product = productRepository.findById(id);
-        return product.get();
+        return modelMapper.map(product.get(), ProductResponse.class);
     }
 }
