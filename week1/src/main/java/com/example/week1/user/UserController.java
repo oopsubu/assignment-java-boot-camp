4764a5fe83d322fbf8145091d7ee4cc0ebf9e6e3
D1-PC <polubu@gmail.com>
2022-02-27 13:40:50 +0700

    feat : Add dependency and config logback to project

--- week1/src/main/java/com/example/week1/user/UserController.java
+++ week1/src/main/java/com/example/week1/user/UserController.java
@@ -1,19 +1,32 @@
 package com.example.week1.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class UserController {
+
+    private static final Logger log
+            = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable int id) {
-//        Optional<User> user = userRepository.findById(id);userResponse.setUserPayments(user.get().getUserPayments());
-        return userRepository.findById(id).get();
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Integer id) {
+        log.info("/user/{}", id);
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        throw new UserNotFoundException("Not found User with id = " + id);
     }
 }
