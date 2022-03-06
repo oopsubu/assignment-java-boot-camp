be7cd29f9d11bb444029d47d80cff015a8f9fe02
Chalermpol-PC <polubu@gmail.com>
2022-03-06 18:58:09 +0700

    fix: Add function search product by name

---
+++ week1/src/main/java/com/example/week1/product/ProductSpecificationRepository.java
@@ -0,0 +1,6 @@
+package com.example.week1.product;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductSpecificationRepository extends JpaSpecificationExecutor<Product> {
+}
