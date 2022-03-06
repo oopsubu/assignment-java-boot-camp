21c2e43d94879cb7a8a1ac66d9e67d4ffa39105e
Chalermpol-PC <polubu@gmail.com>
2022-03-06 18:58:33 +0700

    fix: Add function search product by name

--- week1/src/main/java/com/example/week1/product/ProductRepository.java
+++ week1/src/main/java/com/example/week1/product/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.example.week1.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Set;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
     Set<Product> findByNameContaining(String productName);
 }
